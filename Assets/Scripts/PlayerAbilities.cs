bdc469463d124497e7ab9746a53c146ef0f3b679
Luz4r <piorek.r@gmail.com>
2019-12-09 22:33:29 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerAbilities.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilities : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    private List<GameObject> bullets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullets = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject bullet = Instantiate<GameObject>(bulletPrefab, pos, Quaternion.identity);
+            bullets.Add(bullet);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private Vector3 rotationValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationValue = new Vector3(0.0f, 0.0f, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+        float dirY = Input.GetAxis("Vertical");
+        transform.Translate(dirX * Time.deltaTime * speed, dirY * Time.deltaTime * speed, 0);
+    }
+
+    private void HandleRotation()
+    {
+        Camera.main.orthographicSize += -Input.mouseScrollDelta.y;
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.eulerAngles += rotationValue;
+        }
+        else if(Input.GetKey(KeyCode.E))
+        {
+            transform.eulerAngles -= rotationValue;
+        }
+    }
+}
