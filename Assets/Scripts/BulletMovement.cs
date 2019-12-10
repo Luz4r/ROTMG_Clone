494bba64ab20968e1d4aed75ca9c5c922d64a454
Luz4r <piorek.r@gmail.com>
2019-12-10 01:56:40 +0100

    Spawn bullet, rotate it, make it move

--- Assets/Scripts/BulletMovement.cs
+++ Assets/Scripts/BulletMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class BulletMovement : MonoBehaviour
 {
+    private Vector3 dir;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(dir == null)
+        {
+            dir = Vector3.zero;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += dir;
+    }
+
+    public void setDir(Vector3 dir)
+    {
+        this.dir = dir;
     }
 }
--- Assets/Scripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerAbilities.cs
@@ -6,20 +6,38 @@ public class PlayerAbilities : MonoBehaviour
 {
     public GameObject bulletPrefab;
     private List<GameObject> bullets;
+    private bool isShooting = false;
+    private float time;
+    private float shootingFrequency;
 
     // Start is called before the first frame update
     void Start()
     {
         bullets = new List<GameObject>();
+        time = Time.time;
+        shootingFrequency = 0.3f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        isShooting = Input.GetMouseButton(0);
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Time.time - time > shootingFrequency && isShooting)
         {
+            time = Time.time;
             Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            GameObject bullet = Instantiate<GameObject>(bulletPrefab, pos, Quaternion.identity);
+            Vector2 playerPos = transform.position;
+            Vector2 dir = new Vector2(pos.x - playerPos.x, pos.y - playerPos.y).normalized;
+
+            GameObject bullet = Instantiate<GameObject>(bulletPrefab, playerPos, Quaternion.FromToRotation(Vector2.up, dir));
+
+            bullet.GetComponent<BulletMovement>().setDir(new Vector3(dir.x, dir.y) * 0.1f);
+            Destroy(bullet, 1.0f); //TODO destroy bullet on some range
             bullets.Add(bullet);
         }
     }
